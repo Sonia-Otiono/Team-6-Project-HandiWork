5a257b607d2dedf98e67341fe374ed17493a822f
Sonia <soniaotiono@gmail.com>
2020-12-22 12:09:15 +0100

    Added PHP Logic

---
+++ src/all-courses.php
@@ -0,0 +1,51 @@
+<?php
+
+    include('include/config.php');
+
+    include('include/header.php');
+
+    $sql = "SELECT * FROM courses ORDER by DateCreated DESC";
+
+    $result = mysqli_query($conn, $sql);
+    
+    $courses = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_close($conn);  
+
+?>
+
+
+    <section>
+        <div class="container"><h2>All Courses</h2></div>
+        <div class="container">
+            <div class="row">
+                <?php foreach($courses as $course){ ?>
+                <div class="col s12 m6 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="Images/<?php echo $course['Image'];?>" width="250px" height="250px">
+                        </div>
+                        <div class="card-content">
+                        <div class="card-title">
+                        <h4 class="black-text"><?php echo $course['Title'];?></h4>
+                        </div>
+                            <h5><?php echo $course['Description'];?></h5>
+                            <!-- <h5><?php //echo $course['Content'];?></h5> -->
+                        </div>
+                        <div class="card-action">
+                            <a href="one-course.php?CourseID=<?php echo $course['CourseID'] ?>">VIEW</a>
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>    
+    </section>
+
+
+
+<?php 
+
+    include('include/footer.php'); 
+
+?>
---
+++ src/login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>    
+<html>    
+<head>    
+    <title>Login Form</title>    
+    <link rel="stylesheet" type="text/css" href="style.css">    
+</head>    
+<body>    
+    <h1>Login Page</h1>
+            
+    <div class="login">    
+    
+    <form id="login-form" method="post" action="login.php">   
+        <label><b>User Name     
+        </br>    
+        </label>    
+        <input type="text" name="username" id="username" placeholder="Username">    
+        <br><br>    
+        <label><b>Password     
+        </br>    
+        </label>    
+        <input type="Password" name="password" id="password" placeholder="Password">    
+        <br><br> 
+        <input type="checkbox" id="check">    
+        <span>Remember me</span>    
+        <br><br>   
+        <input type="submit" name="log" id="log" value="Login">       
+        <br><br>    
+        
+        <!--<div>    
+        <a href="#">Forgot Password</a>  
+        </div>-->
+    </form>     
+</div>    
+</body>    
+</html> 
+
+<?php  
+ include('include/config.php');
+
+
+ //If user entered username and password
+if (isset($_POST['username']) and isset($_POST['password']))
+{
+    // Get username and password from Form POST Request and assign to vairables
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //Query to check matching username and password from users table 
+    $query = "SELECT * FROM users WHERE Username='$username' and Password='$password'";
+
+    if(mysqli_query($conn, $query))
+    {
+    //Execute Query
+    $result = mysqli_query($conn, $query);  
+            
+    //Get numbers of rows retrieved 
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1)
+    {
+        //echo "Login Credentials verified";
+        echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+
+        // success
+        header('Location: homepage.php');
+    }
+    else
+    {
+        //echo "Invalid Login Credentials";
+        echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+    }
+        }
+        else{
+            echo "<script type='text/javascript'>alert('Database connection failed')</script>";
+
+        }
+  
+}
+else{
+    echo "failed";
+
+}
+?>
